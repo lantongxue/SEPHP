14255ed2bca5abde5d41ce7e074e0476f91afffd
coding <coding@coding.net>
2017-05-24 06:52:39 +0000

    no msg

---
+++ framework/sephp.php
@@ -0,0 +1,10 @@
+<?php
+
+class SEPHP{
+  public function __construct($config){
+    print_r($config);
+  }
+  public function run(){
+    
+  }
+}
--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo "hello sephp framework.";
+include dirname(__FILE__)."/framework/sephp.php";
+
+$config = array(
+  "app_name" => "Sample",
+  "app_path" => dirname(__FILE__)."/Sample",
+);
+$app = new SEPHP($config);
+$app->run();
